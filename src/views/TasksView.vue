f7d991b2543936e1022a13b384a5d9075a75ddf8
quassar <leonscript@gmail.com>
2023-07-04 19:20:29 +0300

    "update-finish"

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -11,7 +11,7 @@
 
       <v-spacer />
 
-      <v-btn icon @click="openCreateTaskModal">
+      <v-btn v-if="tasksList.length" icon @click="openCreateTaskModal">
         <v-icon>mdi-plus</v-icon>
       </v-btn>
     </v-app-bar>
